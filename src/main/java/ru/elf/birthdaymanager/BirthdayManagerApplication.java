98053400b826101aace2be8d5dca96b51d7cfc50
LAYENS <alex_job94@mail.ru>
2025-10-13 22:00:26 +0700

    0.01 Инициализация.
    Создан проект, добавлена зависимость телеграм.

---
+++ src/main/java/ru/elf/birthdaymanager/BirthdayManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.elf.birthdaymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BirthdayManagerApplication.class, args);
+    }
+
+}
